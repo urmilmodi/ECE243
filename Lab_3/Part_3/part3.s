ac82e54a8245673fea8f73cb2a5735d02470676f
urmilmodi <urmilnmodi@gmail.com>
2020-02-13 01:18:12 -0500

    lab 3 demo

--- Lab_3/Part_3/part3.s
+++ Lab_3/Part_3/part3.s
@@ -2,22 +2,30 @@
 
             .text                   // executable code follows
             .global _start                  
-_start:                             
-            MOV     R4, #RESULT     // R4 points to result location
-            LDR     R0, [R4, #4]    // R0 holds the number of elements in the list
-            MOV     R1, #NUMBERS    // R1 points to the start of the list
-            BL      LARGE           
-            STR     R0, [R4]        // R0 holds the subroutine return value
-
-END:        B       END             
+_start:      
+         MOV      R4, #RESULT   // R4 points to result location
+         LDR      R0, [R4, #4]  // R0 holds number of elements in the list
+         MOV      R1, #NUMBERS  // R1 points to the list of integers
+         LDR      R0, [R1]      // R0 holds the largest number so far
+         MOV      R2, #0
+         MOV      R3, #0
 
 /* Subroutine to find the largest integer in a list
  * Parameters: R0 has the number of elements in the lisst
  *             R1 has the address of the start of the list
  * Returns: R0 returns the largest item in the list
  */
-LARGE:      . . .
-            . . .
+LARGE:    SUBS     R0, #1        // decrement the loop counter
+          BEQ      DONE          // if result is equal to 0, branch
+          ADD      R1, #4
+          LDR      R3, [R1]      // get the next number
+          CMP      R2, R3        // check if larger number found
+          BGE      LARGE
+          MOV      R2, R3        // update the largest number
+          B        LARGE
+DONE:     STR      R2, [R4]      // store largest number into result location
+
+END:     B        END
 
 RESULT:     .word   0           
 N:          .word   7           // number of entries in the list
--- Lab_3/Part_4/part4.s
+++ Lab_3/Part_4/part4.s
@@ -6,18 +6,19 @@ _start:
             MOV    R5, #Digits  // R5 points to the decimal digits storage location
             LDR    R4, [R4]     // R4 holds N
             MOV    R0, R4       // parameter for DIVIDE goes in R0
+            MOV    R1, #Divisor
             BL     DIVIDE
             STRB   R1, [R5, #1] // Tens digit is now in R1
             STRB   R0, [R5]     // Ones digit is in R0
 END:        B      END
 
-/* Subroutine to perform the integer division R0 / 10.
+/* Subroutine to perform the integer division R0 / R1.
  * Returns: quotient in R1, and remainder in R0
 */
 DIVIDE:     MOV    R2, #0
-CONT:       CMP    R0, #10
+CONT:       CMP    R0, R1
             BLT    DIV_END
-            SUB    R0, #10
+            SUB    R0, R1
             ADD    R2, #1
             B      CONT
 DIV_END:    MOV    R1, R2     // quotient in R1 (remainder in R0)
