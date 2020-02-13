917abb9045df22016d907243c5b2048ff4385b4e
urmilmodi <urmilnmodi@gmail.com>
2020-02-13 18:17:27 -0500

    fixed part 4

--- Lab_3/Part_4/part4.s
+++ Lab_3/Part_4/part4.s
@@ -6,14 +6,20 @@ _start:
             MOV    R5, #Digits  // R5 points to the decimal digits storage location
             LDR    R4, [R4]     // R4 holds N
             MOV    R0, R4       // parameter for DIVIDE goes in R0
-            MOV    R1, #10
+
+LOOP:       MOV    R1, #10      // Divide By ten
             BL     DIVIDE
-            STRB   R1, [R5, #1] // Tens digit is now in R1
-            STRB   R0, [R5]     // Ones digit is in R0
+            STRB   R0, [R5], #1
+            MOV    R0, R1
+            CMP    R0, #0
+            BGT    LOOP
+            MOV    R0, #Digits
+            LDR    R0, [R0]
 END:        B      END
 
 /* Subroutine to perform the integer division R0 / R1.
  * Returns: quotient in R1, and remainder in R0
+ * 
 */
 DIVIDE:     MOV    R2, #0
 CONT:       CMP    R0, R1
@@ -24,7 +30,7 @@ CONT:       CMP    R0, R1
 DIV_END:    MOV    R1, R2     // quotient in R1 (remainder in R0)
             MOV    PC, LR
 
-N:          .word  76         // the decimal number to be converted
+N:          .word  7645         // the decimal number to be converted
 Digits:     .space 4          // storage space for the decimal digits
 
             .end
