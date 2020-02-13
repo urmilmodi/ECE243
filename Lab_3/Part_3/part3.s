51517847941ad764ca422b6d6d1f2691f5bebf8c
urmilmodi <urmilnmodi@gmail.com>
2020-02-13 02:07:52 -0500

    fixed part3 used mov instead of str

--- Lab_3/Part_3/part3.s
+++ Lab_3/Part_3/part3.s
@@ -2,13 +2,16 @@
 
             .text                   // executable code follows
             .global _start                  
-_start:      
-         MOV      R4, #RESULT   // R4 points to result location
-         LDR      R0, [R4, #4]  // R0 holds number of elements in the list
-         MOV      R1, #NUMBERS  // R1 points to the list of integers
-         LDR      R0, [R1]      // R0 holds the largest number so far
-         MOV      R2, #0
-         MOV      R3, #0
+_start:                             
+            MOV     R4, #RESULT     // R4 points to result location
+            LDR     R0, [R4, #4]    // R0 holds the number of elements in the list
+            MOV     R1, #NUMBERS    // R1 points to the start of the list
+            MOV     R2, #0
+            MOV     R3, #0
+            BL      LARGE           
+            MOV     R4, R2        // R0 holds the subroutine return value
+
+END:        B       END             
 
 /* Subroutine to find the largest integer in a list
  * Parameters: R0 has the number of elements in the lisst
@@ -16,16 +19,16 @@ _start:
  * Returns: R0 returns the largest item in the list
  */
 LARGE:    SUBS     R0, #1        // decrement the loop counter
-          BEQ      DONE          // if result is equal to 0, branch
-          ADD      R1, #4
-          LDR      R3, [R1]      // get the next number
-          CMP      R2, R3        // check if larger number found
-          BGE      LARGE
-          MOV      R2, R3        // update the largest number
-          B        LARGE
-DONE:     STR      R2, [R4]      // store largest number into result location
-
-END:     B        END
+         BEQ      DONE          // if result is equal to 0, branch
+         ADD      R1, #4
+         LDR      R3, [R1]      // get the next number
+         CMP      R2, R3        // check if larger number found
+         BGE      LARGE
+         MOV      R2, R3        // update the largest number
+         B        LARGE
+DONE:    STR      R2, [R0]      // store largest number into result location
+         BX       LR
+         
 
 RESULT:     .word   0           
 N:          .word   7           // number of entries in the list
